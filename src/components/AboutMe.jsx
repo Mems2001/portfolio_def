b9015c97c07ea25885e77300264622ff57575c8b
Mems2001 <marioed95@hotmail.com>
2023-03-16 12:04:37 -0500

    feat: aboutMe text added

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,18 +4,19 @@ import '../styles/AboutMe.css'
 
 const AboutMe = ({language}) => {
   return (
-    <section className='hidden' id='aboutMe'>
+    <section className='hidden aboutMe_container' id='aboutMe'>
+      <div className='aboutMe hidden_2' id='fewWords'>
+        <h2>{language[1].title}</h2>
+
+        <p>{language[1].content}</p>
+      </div>
+
       <div className='presentation hidden_2' id='presentation'>
         <div></div>
         <div className='photo_container'>
           <div className='personal_photo'/> 
         </div>
         <div></div>
-      </div>
-      <div className='aboutMe hidden_2' id='fewWords'>
-        <h2>{language[1].title}</h2>
-
-        <p>{language[1].content}</p>
       </div>
     </section>
   )
