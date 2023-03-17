728652c0bdaf72e2d829ce3c2b972f56c0fbe029
Mems2001 <marioed95@hotmail.com>
2023-03-17 03:54:12 -0500

    fix: more tests

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -40,14 +40,14 @@ const Contact = ({language}) => {
     <section className='hidden' id='contact'>
       <h2 className='section_title'>{language[5].title}</h2>
 
-      <form name='contact' className='form' onSubmit={handleSubmit(submit)}>
+      <form name='contactC' className='form' onSubmit={handleSubmit(submit)}>
         <label htmlFor='nameC'>{language[5].content.name.title}:</label>
-        <input type='text' name='name' id='nameC' {...register('name_contact')} placeholder={language[5].content.name.placeholder}/>
+        <input type='text' name='nameC' id='nameC' {...register('name_contact')} placeholder={language[5].content.name.placeholder}/>
         <label htmlFor='emailC'>{language[5].content.email.title}:</label>
-        <input type='email' name='email' id='emailC' {...register('email_contact')} placeholder={language[5].content.email.placeholder}/>
+        <input type='email' name='emailC' id='emailC' {...register('email_contact')} placeholder={language[5].content.email.placeholder}/>
         <label htmlFor='messageC'>{language[5].content.message.title}:</label>
-        <textarea name='message' id='messageC' {...register('message_contact')} placeholder={language[5].content.message.placeholder} rows='10'/>
-        <button className='form_btn'>{language[5].content.submit}</button>
+        <textarea name='messageC' id='messageC' {...register('message_contact')} placeholder={language[5].content.message.placeholder} rows='10'/>
+        <button type='submit' className='form_btn'>{language[5].content.submit}</button>
         <input type="hidden" name="form-name" value="contact" />
       </form>
     </section>
