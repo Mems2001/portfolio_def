03f184c7644a297ca99de095ecf171300ac461ad
Mems2001 <mems2001code@gmail.com>
2025-07-25 03:28:28 -0500

    update: small design changes 8

--- src/components/SkillCard.jsx
+++ src/components/SkillCard.jsx
@@ -3,11 +3,11 @@ const SkillCard = ({skill}) => {
   if (skill.skills.length > 0) {
     return (
       <div className='logo_container hidden_2'>
-        <h2 className='skill_name'>{skill.name}</h2>
-  
         <div className='logo_container_2'>
           <img className='logo_image' src={skill.image}/>
         </div>
+  
+        <h2 className='skill_name'>{skill.name}</h2>
 
         <div className='skills_list'>
           {skill.skills.map(element => 
